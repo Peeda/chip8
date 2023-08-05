dfaedc15f1e88967018d91be19b49c7b7b45999f
Peeda <107806301+Peeda@users.noreply.github.com>
2023-08-05 13:52:16 -0400

    Initial implementations done

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,6 +1,9 @@
 use std::io::prelude::*;
 use std::cmp;
 use std::fs::File;
+use std::mem;
+
+use rand::Rng;
 const RAM_SIZE:usize = 4096;
 const SCREEN_WIDTH:usize = 64;
 const SCREEN_HEIGHT:usize = 32;
@@ -24,18 +27,33 @@ const FONT_DATA:[u8;16*5] = [
     0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 ];
 const PROGRAM_START:usize = 0x200;
+const FONT_START:usize = 0x050;
+pub enum Version {
+    Original,
+    Schip,
+    XoChip,
+}
 pub struct Chip8 {
     ram:[u8;RAM_SIZE],
     pc:u16,
     pub screen_state:[bool;SCREEN_WIDTH*SCREEN_HEIGHT],
     i_reg:u16,
-    v_reg:[u16;REGISTERS],
+    v_reg:[u8;REGISTERS],
     stack:Vec<u16>,
     delay_timer:u8,
     sound_timer:u8,
+    pub file_loaded:bool,
+    pub input_data:[bool;16],
+    pub prev_input_data:[bool;16],
+    quirks:[bool;13],
+}
+impl Default for Chip8 {
+    fn default() -> Chip8 {
+        Chip8::new(Version::Schip)
+    }
 }
 impl Chip8 {
-    pub fn new() -> Chip8 {
+    pub fn new(version:Version) -> Chip8 {
         let mut chip8 = Chip8 {
             ram:[0;RAM_SIZE],
             pc:PROGRAM_START as u16,
@@ -45,20 +63,34 @@ impl Chip8 {
             stack:Vec::new(),
             delay_timer:0,
             sound_timer:0,
+            file_loaded:false,
+            input_data:[false;16],
+            prev_input_data:[false;16],
+            quirks:[false;13],
         };
-        const FONT_START:usize = 0x050;
         for (i,font_byte) in FONT_DATA.iter().enumerate() {
             chip8.ram[FONT_START + i] = *font_byte;
+        }
+        let indices = match version {
+            Version::Original => vec![4,8],
+            Version::Schip => vec![5,9,11],
+            Version::XoChip => vec![6]
+        };
+        for index in indices {
+            chip8.quirks[index] = true;
         }
         chip8
     }
     pub fn read_file(&mut self, path:&str) {
-        let rom:File = File::open(path).expect("Failed to read file");
-        let mut ind = PROGRAM_START;
-        for byte in rom.bytes() {
-            self.ram[ind] = byte.expect("Failed to read byte from file");
-            ind += 1;
-        }
+        if !self.file_loaded {
+            let rom:File = File::open(path).expect("Failed to read file");
+            let mut ind = PROGRAM_START;
+            for byte in rom.bytes() {
+                self.ram[ind] = byte.expect("Failed to read byte from file");
+                ind += 1;
+            }
+            self.file_loaded = true;
+        } else {println!("tried to read file after load")}
     }
     pub fn step(&mut self) {
         let higher_byte:u16 = self.ram[self.pc as usize] as u16;
@@ -66,37 +98,108 @@ impl Chip8 {
         let opcode:u16 = higher_byte << 8 | lower_byte;
         let nibbles = ((opcode & 0xF000) >> 12, (opcode & 0x0F00) >> 8, 
                        (opcode & 0x00F0) >> 4, opcode & 0x000F);
-        let x = (opcode & 0x0F00) >> 8;
-        let y = (opcode & 0x00F0) >> 4;
-        let n = opcode & 0x000F;
-        let nn = opcode & 0x00FF;
+        let x = nibbles.1 as usize;
+        let y = nibbles.2 as usize;
+        let n = nibbles.3 as u8;
+        let nn = (opcode & 0x00FF) as u8;
         let nnn = opcode & 0x0FFF;
         self.pc += 2;
         match nibbles {
             (0,0,0xE,0) => {
-                for i in 0..self.screen_state.len() {
-                    self.screen_state[i] = false;
+                for bit in self.screen_state.iter_mut() {
+                    *bit = false;
                 }
             }
+            (0,0,0xE,0xE) => self.pc = self.stack.pop().expect("Tried to pop empty stack"),
             (1,_,_,_) => self.pc = nnn,
-            (6,_,_,_) => self.v_reg[x as usize] = nn,
-            (7,_,_,_) => self.v_reg[x as usize] += nn,
-            (0xA,_,_,_) => {
-                self.i_reg = nnn;
+            (2,_,_,_) => {
+                self.stack.push(self.pc);
+                self.pc = nnn;
+            }
+            (3,_,_,_) => if self.v_reg[x] == nn {self.pc += 2},
+            (4,_,_,_) => if self.v_reg[x] != nn {self.pc += 2},
+            (5,_,_,0) => if self.v_reg[x] == self.v_reg[y] {self.pc += 2},
+            (6,_,_,_) => self.v_reg[x] = nn,
+            (7,_,_,_) => self.v_reg[x] = self.v_reg[x].wrapping_add(nn),
+            (8,_,_,0) => self.v_reg[x] = self.v_reg[y],
+            (8,_,_,1|2|3) => {
+                match nibbles.3 {
+                    1 => self.v_reg[x] |= self.v_reg[y],
+                    2 => self.v_reg[x] &= self.v_reg[y],
+                    3 => self.v_reg[x] ^= self.v_reg[y],
+                    _ => ()
+                }
+                if self.quirks[4] {self.v_reg[0xF_usize] = 0};
+            }
+            (8,_,_,4) => {
+                let sum = self.v_reg[x].wrapping_add(self.v_reg[y]);
+                if sum < self.v_reg[x] {
+                    self.v_reg[0xF_usize] = 1;
+                } else {
+                    self.v_reg[0xF_usize] = 0;
+                }
+                self.v_reg[x] = sum;
+            }
+            (8,_,_,5 | 7) => {
+                let mut first = self.v_reg[x];
+                let mut second = self.v_reg[y];
+                if nibbles.3 == 7 {
+                    mem::swap(&mut first, &mut second);
+                }
+                let sub = first.wrapping_sub(second);
+                if sub > first {
+                    self.v_reg[0xF_usize] = 1;
+                } else {
+                    self.v_reg[0xF_usize] = 0;
+                }
+                self.v_reg[x] = sub;
+            }
+            (8,_,_,6 | 0xE) => {
+                if !self.quirks[5] {
+                    self.v_reg[x] = self.v_reg[y];
+                }
+                match nibbles.3 {
+                    6 => {
+                        self.v_reg[0xF_usize] = self.v_reg[x] & 1;
+                        self.v_reg[x] >>= 1;
+                    }
+                    0xE => {
+                        self.v_reg[0xF_usize] = self.v_reg[x] & (1 << 7);
+                        self.v_reg[x] <<= 1;
+                    }
+                    _ => ()
+                }
+            }
+            (9,_,_,0) => if self.v_reg[x] != self.v_reg[y] {self.pc += 2}
+            (0xA,_,_,_) => self.i_reg = nnn,
+            (0xB,_,_,_) => {
+                let temp = true;
+                if temp {
+                    /* BNNN instruction */
+                    self.pc = nnn + self.v_reg[0] as u16;
+                } else {
+                    /* BXNN insruction */
+                    self.pc = nnn + self.v_reg[x] as u16;
+                }
+            }
+            (0xC,_,_,_) => {
+                let mut rng = rand::thread_rng();
+                let rand_num:u8 = rng.gen();
+                self.v_reg[x] = rand_num & nn;
             }
             (0xD,_,_,_) => {
-                const WIDTH:u16 = 8;
+                const WIDTH:u8 = 8;
                 self.v_reg[0xF_usize] = 0;
-                let img_x = self.v_reg[x as usize] % 64;
-                let img_y = self.v_reg[y as usize] % 64;
+                let img_x = self.v_reg[x] % 64;
+                let img_y = self.v_reg[y] % 64;
 
-                let right_end = cmp::min(n + img_y,SCREEN_HEIGHT as u16);
-                let bottom_end = cmp::min(WIDTH + img_x,SCREEN_WIDTH as u16);
+                let right_end = cmp::min(n + img_y,SCREEN_HEIGHT as u8);
+                let bottom_end = cmp::min(WIDTH + img_x,SCREEN_WIDTH as u8);
                 for row in img_y..right_end {
-                    let sprite_addr = (self.i_reg + row - img_y) as usize;
+                    let sprite_addr = (self.i_reg + row as u16 - img_y as u16) as usize;
                     let row_data:u8 = self.ram[sprite_addr];
                     for col in img_x..bottom_end{
-                        let ind:usize = (row * SCREEN_WIDTH as u16 + col) as usize;
+                        let ind:usize = (row as u16 * SCREEN_WIDTH as u16 + col as u16) as usize;
                         let shift = (WIDTH - 1) - (col - img_x);
                         if row_data & (1 << shift) > 0 {
                             if self.screen_state[ind] {
@@ -107,6 +210,53 @@ impl Chip8 {
                             }
                         }
                     }
+                }
+            }
+            (0xE,_,9,0xE) => if self.input_data[self.v_reg[x] as usize] {self.pc += 2},
+            (0xE,_,0xA,1) => if !self.input_data[self.v_reg[x] as usize] {self.pc += 2},
+            (0xF,_,0,7) => self.v_reg[x] = self.delay_timer,
+            (0xF,_,0,0xA) => {
+                let mut keycode:Option<u8> = None;
+                for (ind,held) in self.input_data.iter().enumerate() {
+                    if !self.prev_input_data[ind] && *held {
+                        keycode = Some(ind.try_into().expect("keycode > 15"));
+                    }
+                }
+                if let Some(k) = keycode {
+                    self.v_reg[x] = k;
+                } else {
+                    self.pc -= 2;
+                }
+            }
+            (0xF,_,1,5) => self.delay_timer = self.v_reg[x],
+            (0xF,_,1,8) => self.sound_timer = self.v_reg[x],
+            (0xF,_,1,0xE) => self.i_reg += self.v_reg[x] as u16,
+            (0xF,_,2,9) => {
+                let nibble:u8 = self.v_reg[x] & 0xF;
+                self.i_reg = FONT_START as u16 + 5_u16 * nibble as u16;
+            }
+            (0xF,_,3,3) => {
+                let mut val = self.v_reg[x];
+                let ones = val % 10;
+                val -= ones;
+                let tens = val % 100;
+                let hundreds = val - tens;
+                assert!(tens % 10 == 0);
+                assert!(hundreds % 100 == 0);
+                self.ram[self.i_reg as usize] = hundreds/100;
+                self.ram[(self.i_reg + 1) as usize] = tens/10;
+                self.ram[(self.i_reg + 2) as usize] = ones;
+            }
+            (0xF,_,5 | 6,5) => {
+                for i in 0..(x+1) {
+                    if nibbles.2 == 5 {
+                        self.ram[self.i_reg as usize + i] = self.v_reg[i];
+                    } else {
+                        self.v_reg[i] = self.ram[self.i_reg as usize + i];
+                    }
+                }
+                if self.quirks[11] {
+                    self.i_reg += (x+1) as u16;
                 }
             }
             _ => println!("unimplemented opcode {:#06X}", opcode),
@@ -137,14 +287,49 @@ mod tests {
             0x28, 0x8E, 0xA8, 0xA8, 0xA6, 0xCE, 0x87, 0x03, 0x03, 0x03, 0x87, 0xFE, 0xFC, 0x00, 0x00, 0x60, 
             0x90, 0xF0, 0x80, 0x70
         ];
-        let mut hard_coded = Chip8::new();
-        let mut from_file = Chip8::new();
+        let mut hard_coded = Chip8::default();
+        let mut from_file = Chip8::default();
         for i in 0..LOGO.len() {
             hard_coded.ram[PROGRAM_START + i] = LOGO[i];
         }
         from_file.read_file("roms/1-chip8-logo.ch8");
         for i in 0..RAM_SIZE {
             assert_eq!(hard_coded.ram[i],from_file.ram[i]);
+        }
+    }
+    #[test]
+    fn test_quirk_assignment_original() {
+        let chip8 = Chip8::new(Version::Original);
+        assert!(chip8.quirks[4]);
+        assert!(chip8.quirks[8]);
+    }
+    #[test]
+    fn test_quirk_assignment_schip() {
+        let chip8 = Chip8::new(Version::Schip);
+        assert!(chip8.quirks[5]);
+        assert!(chip8.quirks[9]);
+        assert!(chip8.quirks[11]);
+    }
+    #[test]
+    fn test_quirk_assignment_xo() {
+        let chip8 = Chip8::new(Version::XoChip);
+        assert!(chip8.quirks[6]);
+    }
+    #[test]
+    fn test_font_data() {
+        for i in 0..16 {
+            let mut chip8 = Chip8::default();
+            //call FX29 and point to char i
+            chip8.v_reg[0] = i;
+            //make the upper nibble point to register 0 each time
+            chip8.ram[PROGRAM_START] = 0xF0;
+            chip8.ram[PROGRAM_START + 1] = 0x29;
+            chip8.step();
+            //assert that location of I matches the font data
+            for k in 0..5 {
+                assert_eq!(chip8.ram[chip8.i_reg as usize + k],
+                           FONT_DATA[i as usize * 5 + k]);
+            }
         }
     }
 }
--- src/frontend.rs
+++ src/frontend.rs
@@ -1,8 +1,8 @@
 use crate::chip8::Chip8;
 
-use std::{thread,time};
+use std::thread;
 
-use sdl2::pixels::{Color,PixelFormatEnum};
+use sdl2::pixels::PixelFormatEnum;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
@@ -10,34 +10,79 @@ use std::time::Duration;
 const SCALE:u32 = 20;
 const INSTRUCTIONS_PER_SEC:u16 = 600;
 pub fn run() {
-    let mut chip8 = Chip8::new();
-    chip8.read_file("roms/2-ibm-logo.ch8");
+    let mut chip8 = Chip8::default();
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
-
     let window = video_subsystem.window("Rust CHIP-8 Emulator", 64*SCALE, 32*SCALE)
         .opengl()
         .position_centered()
         .build()
         .unwrap();
-
     let mut canvas = window.into_canvas().present_vsync().build().unwrap();
-
     let creator = canvas.texture_creator();
     let mut texture = creator.create_texture_target(PixelFormatEnum::RGB24,64,32).unwrap();
 
     let mut color_buffer = [0_u8; 3 * 64 * 32];
-
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.present();
     let mut event_pump = sdl_context.event_pump().unwrap();
     'running: loop {
+        
+        /* write to the previous input data field */
+        for (ind,held) in chip8.input_data.iter().enumerate() {
+            chip8.prev_input_data[ind] = *held;
+        }
+
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                /* file loading keys */
+                Event::KeyDown {keycode: Some(Keycode::Y), ..} => {
+                    chip8.read_file("roms/1-chip8-logo.ch8");
+                }
+                Event::KeyDown {keycode: Some(Keycode::U), ..} => {
+                    chip8.read_file("roms/2-ibm-logo.ch8");
+                }
+                Event::KeyDown {keycode: Some(Keycode::I), ..} => {
+                    chip8.read_file("roms/3-corax+.ch8");
+                }
+                Event::KeyDown {keycode: Some(Keycode::O), ..} => {
+                    chip8.read_file("roms/4-flags.ch8");
+                }
+                Event::KeyDown {keycode: Some(Keycode::P), ..} => {
+                    chip8.read_file("roms/5-quirks.ch8");
+                }
+                Event::KeyDown {keycode: Some(Keycode::LeftBracket), ..} => {
+                    chip8.read_file("roms/6-keypad.ch8");
+                }
+                Event::KeyDown {keycode: Some(Keycode::RightBracket), ..} => {
+                    chip8.read_file("roms/Space_Invaders.ch8");
+                }
+                Event::KeyDown {keycode: Some(Keycode::Space), ..} => {
+                    chip8 = Chip8::default();
+                }
+                /* input keys */
+                Event::KeyDown {keycode: Some(key), ..} => {
+                    if let Some(ind) = get_button(key) {
+                        chip8.input_data[ind as usize] = true;
+                    }
+                }
+                Event::KeyUp {keycode: Some(key), ..} => {
+                    if let Some(ind) = get_button(key) {
+                        chip8.input_data[ind as usize] = false;
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        if chip8.file_loaded {chip8.step()};
+
         for color in color_buffer.iter_mut().take(3*64*32) {
             *color = 0;
         }
-
-        chip8.step();
 
         for i in 0..64*32 {
             if chip8.screen_state[i] {
@@ -48,19 +93,31 @@ pub fn run() {
         }
 
         canvas.clear();
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
-                _ => {}
-            }
-        }
 
         texture.update(None, &color_buffer, 3 * 64).expect("Failed texture update");
         canvas.copy(&texture,None,None).unwrap();
         canvas.present();
-        thread::sleep(Duration::from_millis(100));
+        // thread::sleep(Duration::from_millis(50));
+    }
+    fn get_button(key: Keycode) -> Option<u16> {
+        match key {
+            Keycode::Num1 => Some(0x1),
+            Keycode::Num2 => Some(0x2),
+            Keycode::Num3 => Some(0x3),
+            Keycode::Num4 => Some(0xC),
+            Keycode::Q    => Some(0x4),
+            Keycode::W    => Some(0x5),
+            Keycode::E    => Some(0x6),
+            Keycode::R    => Some(0xD),
+            Keycode::A    => Some(0x7),
+            Keycode::S    => Some(0x8),
+            Keycode::D    => Some(0x9),
+            Keycode::F    => Some(0xE),
+            Keycode::Z    => Some(0xA),
+            Keycode::X    => Some(0x0),
+            Keycode::C    => Some(0xB),
+            Keycode::V    => Some(0xF),
+            _ => None,
+        }
     }
 }
